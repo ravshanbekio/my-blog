5c1c2672f75a3ad7974b3b584b64ee84a8918ed5
Ravshanbek <88030420+ravshanbekio@users.noreply.github.com>
2023-06-04 12:30:48 +0300

    Added celery conf to celery.py file

--- config/celery.py
+++ config/celery.py
@@ -4,5 +4,7 @@ from celery import Celery
 os.environ.setdefault('DJANGO_SETTINGS_MODULE','config.settings')
 
 celery = Celery('config')
+celery.conf.broker_url = 'redis://localhost:6379/0'
+celery.conf.result_backend = 'redis://localhost:6379/0'
 celery.config_from_object('django.conf:settings',namespace="CELERY")
 celery.autodiscover_tasks()
